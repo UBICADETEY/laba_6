31e776934ab8545dffdf2d3e0a75a83f8c5d92fb
микола тесла <chemurziev.mikail@mail.ru>
2020-12-17 14:07:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace laba_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+        TextWriter save_out=Console.Out;
+        TextReader save_in = Console.In;
+        var new_out = new StreamWriter(@"par_output.txt");
+        var new_in = new StreamReader (@"par_input.txt");
+        Console.SetOut(new_out);
+        Console.SetIn(new_in);
+#endif
+            lines l = new lines ();
+            l.Load();
+            l.Info();
+
+
+
+#if !DEBUG
+        Console.SetOut(save_out); new_out.Close();
+        Console.SetIn(save_in); new_in.Close();
+
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
---
+++ lines.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace laba_6
+{
+    class lines
+    {
+        private double x1,x2,y1,y2;
+
+        public double Getd() { return  (Math.Sqrt( Math.Pow( x2-x1,2)+ Math.Pow(y2-y1,2))); }
+        public double Getd1() { return (Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2 , 2))); }
+
+        public double Getd2() { return (Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2))); }
+
+
+        public void Info()
+        {
+            String str = "*******" +
+                "отрезок";
+            Console.WriteLine(str);
+            Console.WriteLine(" координаты начала отрезка " + x1 + " " + y1);
+            Console.WriteLine(" координаты конца отрезка " + x2 + " " + y2);
+            Console.WriteLine(string.Format("d = {0:0.00}", Getd()));
+            Console.WriteLine(string.Format("d1  = {0:0.00}", Getd1()));
+            Console.WriteLine(string.Format("d2 = {0:0.00}", Getd2()));
+
+
+        }
+
+        public void Load()
+        {
+            x1 = Convert.ToDouble(Console.ReadLine());
+             y1 = Convert.ToDouble(Console.ReadLine());
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
